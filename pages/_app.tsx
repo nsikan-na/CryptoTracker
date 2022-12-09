520ecd62099fdaf9b0d26d56332a9c57257795a8
Nsikan Akpan <nsikan.akpan@betmakers.com>
2022-12-09 15:28:21 -0500

    improve seo

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,6 +5,8 @@ import { createContext, useState, useEffect } from "react";
 import { UserProvider } from "@auth0/nextjs-auth0";
 import toggleTheme from "../util/toggleTheme";
 import Alert from "../components/Alert";
+import Head from "next/head";
+
 export const Context = createContext({});
 function MyApp({ Component, pageProps }: AppProps) {
   const [currency, setCurrency] = useState("USD");
@@ -15,6 +17,21 @@ function MyApp({ Component, pageProps }: AppProps) {
   }, [theme]);
   return (
     <UserProvider>
+      <Head>
+        <title>CryptoTracker</title>
+        <meta
+          name="description"
+          content="CryptoTracker Nsikan Akpan"
+          key="desc"
+        />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta property="og:title" content="CryptoTracker Nsikan Akpan" />
+        <meta property="og:description" content="CryptoTracker Nsikan Akpan" />
+        <meta name="robots" content="all" />
+        <meta name="googlebot" content="noindex,nofollow" />
+        <meta name="google" content="nositelinkssearchbox" key="sitelinks" />
+        <meta name="google" content="notranslate" key="notranslate" />
+      </Head>
       <Context.Provider
         value={{
           currency,
@@ -26,7 +43,7 @@ function MyApp({ Component, pageProps }: AppProps) {
         }}
       >
         <Component {...pageProps} />
-        <Alert  />
+        <Alert />
       </Context.Provider>
     </UserProvider>
   );
